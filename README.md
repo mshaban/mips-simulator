a23d947934d253bdaa5b89f883f7e3be99e657f5
shaban <mohamed19936@gmail.com>
2014-05-21 02:52:35 +0200

    adding project files

---
+++ README.md
@@ -0,0 +1,5 @@
+# this is mips simulator made as a school project
+
+made by
+1. Mohamed Shaban
+2. Eslam Maged
---
+++ src/Default/Main.java
@@ -0,0 +1,17 @@
+package Default;
+
+/**
+ * Created by mohamed on 5/21/14.
+ * package Default
+ * project mips-simulator
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        String [] arr = {"mohamed", "shaban"};
+        for(String s: arr){
+
+        }
+    }
+}
