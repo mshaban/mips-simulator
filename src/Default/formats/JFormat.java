f5f39f39164148eb09ca3e503aff4912c53d20f4
shaban <mohamed19936@gmail.com>
2014-05-22 22:53:13 +0200

    add format classes

---
+++ src/Default/formats/JFormat.java
@@ -0,0 +1,9 @@
+package Default.formats;
+
+/**
+ * Created by mohamed on 5/22/14.
+ * package Default.formats
+ * project mips-simulator
+ */
+public class JFormat {
+}
---
+++ src/Default/formats/RFormat.java
@@ -0,0 +1,10 @@
+package Default.formats;
+
+/**
+ * Created by mohamed on 5/22/14.
+ * package Default.formats
+ * project mips-simulator
+ */
+public class RFormat {
+
+}
