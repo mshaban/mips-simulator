e799195d2830468c1d6eeab30cf16e71701237ac
shaban <mohamed19936@gmail.com>
2014-05-22 23:27:07 +0200

    add i format

--- src/formats/IFormat.java
+++ src/formats/IFormat.java
@@ -1,12 +1,29 @@
 package formats;
 
+import exceptions.InvalidArgumentException;
+import exceptions.OutOfRangeException;
+
 /**
  * Created by mohamed on 5/22/14.
  * package formats
  * project mips-simulator
  */
-public class IFormat {
+public abstract class IFormat {
 
+    String rs, rt;
+    int constant;
 
+    public IFormat(String rs, String rt, int constant) throws Exception {
+        if (!rs.startsWith("$") || !rt.startsWith("$"))
+            throw new InvalidArgumentException();
+        if (constant > (1 << 15) - 1 || constant < -1 * (1 << 15))
+            throw new OutOfRangeException();
+
+        this.rs = rs;
+        this.rt = rt;
+        this.constant = constant;
+    }
+
+    public abstract void execute();
 }
 
