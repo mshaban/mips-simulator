53b15425294b1b4108ed4b6d842aa004355b25df
shaban <mohamed19936@gmail.com>
2014-05-22 23:41:45 +0200

    add jformat

--- src/formats/JFormat.java
+++ src/formats/JFormat.java
@@ -1,9 +1,19 @@
 package formats;
 
+import exceptions.OutOfRangeException;
+
 /**
  * Created by mohamed on 5/22/14.
  * package formats
  * project mips-simulator
  */
 public class JFormat {
+
+    int address;
+
+    public JFormat(int address) throws Exception {
+        if (address > (1 << 26))
+            throw new OutOfRangeException();
+        this.address = address;
+    }
 }
