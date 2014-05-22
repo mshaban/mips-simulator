a46b78b07b0971c3798193c675e9b145741ea8f7
Eslamunto <eslam.shala@gmail.com>
2014-05-23 00:48:03 +0200

    Add RFormat

--- src/formats/RFormat.java
+++ src/formats/RFormat.java
@@ -1,11 +1,35 @@
 package formats;
 
+import exceptions.InvalidArgumentException;
+import exceptions.OutOfRangeException;
+
 /**
  * Created by mohamed on 5/22/14.
  * package formats
  * project mips-simulator
  */
-public class RFormat {
+public abstract class RFormat {
 
+    private String rs;
+    private String rt;
+    private String rd;
+
+    /**
+     * Constructor
+     * @param rs String
+     * @param rt String
+     * @param rd String
+     *
+     * @author: Eslam Maged
+     */
+    public RFormat( String rs, String rt, String rd) throws  InvalidArgumentException {
+        if((!(rs.trim().charAt(0)!='$') )||(!(rt.trim().charAt(0)!='$')))
+            throw new InvalidArgumentException();
+        this.rs = rs;
+        this.rt = rt;
+        this.rd = rd;
+    }
+
+    public abstract void execute();
 
 }
